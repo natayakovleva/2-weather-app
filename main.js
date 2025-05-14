e129a688852d4a51e9c5cd518a7c8078d5297863
Natalia Yakovleva <yakovlevane29@gmail.com>
2025-05-14 16:09:28 +0300

    added code in main.js

---
+++ main.js
@@ -0,0 +1,43 @@
+const apiKey = "e349553e4be1f3a108798ec32604942e";
+const apiUrl =
+  "https://api.openweathermap.org/data/2.5/weather?units=metric&q=";
+const searchBox = document.querySelector(".search input");
+const searchBtn = document.querySelector(".search button");
+const weatherIcon = document.querySelector(".weather-icon");
+const weatherIcons = {
+  Clouds: "image/clouds.png",
+  Rain: "image/rain.png",
+  Drizzle: "image/drizzle.png",
+  Clear: "image/clear.png",
+  Mist: "image/mist.png",
+};
+
+async function checkWeather(city) {
+  const response = await fetch(apiUrl + city + `&appid=${apiKey}`);
+
+  if (response.status == 404) {
+    document.querySelector(".error").style.display = "block";
+    document.querySelector(".weather").style.display = "none";
+  } else {
+    let data = await response.json();
+
+    document.querySelector(".city").innerHTML = data.name;
+    document.querySelector(".term").innerHTML =
+      Math.round(data.main.temp) + "°C";
+    document.querySelector(".humidity").innerHTML = data.main.humidity + "%";
+    document.querySelector(".wind").innerHTML = data.wind.speed + " km/h";
+
+    const weatherMain = data.weather[0].main;
+
+    if (weatherIcons[weatherMain]) {
+      weatherIcon.src = weatherIcons[weatherMain];
+    }
+
+    document.querySelector(".weather").style.display = "block";
+    document.querySelector(".error").style.display = "none";
+  }
+}
+
+searchBtn.addEventListener("click", () => {
+  checkWeather(searchBox.value);
+});
